7d2facb516d748e60898458bb463652aa81a627d
Martin Tsang <martin.tsang.technine@gmail.com>
2023-06-27 17:00:32 -0400

    responsive ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import experiences from '../constants/expereinces'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center py-24 px-96">
-      <div className="relative w-full grid grid-cols-2 gap-4">
-        <div>
-          <div className="sticky h-[85vh] top-24 flex flex-col justify-between">
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <main className="flex flex-col items-center">
+        <div className="relative lg:flex lg:justify-between lg:gap-4">
+          <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
             <div className='flex flex-col'>
               <p className="text-5xl font-bold">
                 Martin Tsang
@@ -17,7 +17,7 @@ export default function Home() {
                 Software Engineer at Apoidea
               </p>
             </div>
-            <div className="grid grid-cols-8">
+            <div className="grid grid-cols-8 mt-12">
               <a href="https://github.com/marttsang2">
                 <Image src="/icons/github.svg" alt="github icon" width={32} height={32} />
               </a>
@@ -25,26 +25,31 @@ export default function Home() {
                 <Image src="/icons/linkedin.svg" alt="linkedin icon" width={32} height={32} />
               </a>
             </div>
+          </header>
+          <div className='pt-12 lg:w-1/2 lg:py-24'>
+            <p className="text-medium font-medium my-2 uppercase font-slate-200">
+              Experience
+            </p>
+            <li className="flex flex-col group/list">
+              {
+                experiences.map((experience) => (
+                  <ol key={experience.title} className="lg:mb-12">
+                    <ExperienceCard
+                      title={experience.title}
+                      company={experience.company}
+                      startDate={experience.startDate}
+                      endDate={experience.endDate}
+                      skills={experience.skills}
+                      description={experience.description}
+                      url={experience.url}
+                    />
+                  </ol>
+                ))
+              }
+            </li>
           </div>
         </div>
-        <li className="flex flex-col group/list">
-          {
-            experiences.map((experience) => (
-              <ol key={experience.title} className="mb-12">
-                <ExperienceCard
-                  title={experience.title}
-                  company={experience.company}
-                  startDate={experience.startDate}
-                  endDate={experience.endDate}
-                  skills={experience.skills}
-                  description={experience.description}
-                  url={experience.url}
-                />
-              </ol>
-            ))
-          }
-        </li>
-      </div>
-    </main>
+      </main>
+    </div>
   )
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -12,8 +12,8 @@ type ExperienceCardProps = {
 
 export const ExperienceCard = ({ title, company, startDate, endDate, description, skills, url }: ExperienceCardProps) => (
     <div className="group relative transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50 border-[1px] rounded-lg border-transparent hover:border-sky-400/40">
-        <a className="m-3 grid pb-1 sm:grid-cols-12 sm:gap-8" href={url}>
-            <header className="mt-1 col-span-4 text-xs uppercase tracking-wide text-slate-500">{startDate} — {endDate}</header>
+        <a className="m-3 grid pb-1 sm:grid-cols-12 sm:gap-8 lg:gap-0" href={url}>
+            <header className="mt-1 lg:col-span-4 sm:col-span-12 text-xs uppercase tracking-wide text-slate-500">{startDate} — {endDate}</header>
             <div className="flex flex-col col-span-8">
                 <p className="text-medium font-medium text-slate-200">{title} · {company}</p>
                 <p className="text-sm text-slate-500">{description}</p>
