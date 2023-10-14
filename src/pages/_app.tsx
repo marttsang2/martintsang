9b4fd9dc6e698d986cc052677f17bc9187500de6
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-13 21:11:19 -0400

    add custom error page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,24 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-import { useState } from 'react'
-import LoadingScreen from '@/components/Loading/LoadingScreen'
 import Layout from '@/components/Layout/Layout'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-  
+  const router = useRouter();
+  console.log(router);
+  if(router.pathname === '/_error') return <Component {...pageProps} />;
+
   return (
-    <>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <ThemeProvider attribute='class' defaultTheme='light'>
-          <div className='min-h-screen bg-slate-50 dark:bg-slate-800'>
-            <Head>
-              <title>{`Martin's Personal Website`}</title>
-            </Head>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </div>
-        </ThemeProvider>
-      )}
-    </>
+    <ThemeProvider attribute='class' defaultTheme='light'>
+      <div className='min-h-screen bg-slate-50 dark:bg-slate-800'>
+        <Head>
+          <title>{`Martin's Personal Web`}</title>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </ThemeProvider>
   );
 }
