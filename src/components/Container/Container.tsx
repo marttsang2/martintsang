be45af340b511e540b6bde6ce2f06748f82f92f4
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-22 12:35:18 -0400

    fix container tailwind css template string issue

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,14 @@
 type ContainerProps = {
     children: React.ReactNode;
-    maxWidth: 'sm' | 'md' | 'lg' | 'xl';
+    maxWidth: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
     paddingX?: number;
 }
 
-const Container = ({ children, maxWidth, paddingX }: ContainerProps) => {
+
+
+const Container = ({ children, maxWidth }: ContainerProps) => {
   return (
-    <div className={'mx-auto max-w-5xl 2xl:w-4/5 w-11/12'}>
+    <div className={`mx-auto max-w-${maxWidth} 2xl:w-4/5 w-11/12`}>
       {children}
     </div>
   )
