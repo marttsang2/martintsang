395a629754846c5b158bad6f05a694e86423d9f5
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-23 11:18:27 -0400

    fix container width className not working

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -9,8 +9,18 @@ type ContainerProps = {
 
 
 const Container = ({ children, maxWidth }: ContainerProps) => {
+  const maxWidthCSSMapping = {
+    sm: 'max-w-sm',
+    md: 'max-w-md',
+    lg: 'max-w-lg',
+    xl: 'max-w-xl',
+    '2xl': 'max-w-2xl',
+    '3xl': 'max-w-3xl',
+    '4xl': 'max-w-4xl',
+    '5xl': 'max-w-5xl',
+  };
   return (
-    <div className={cn('mx-auto 2xl:w-4/5 w-11/12', `max-w-${maxWidth}`)}>
+    <div className={cn('mx-auto 2xl:w-4/5 w-11/12', maxWidthCSSMapping[maxWidth])}>
       {children}
     </div>
   )
