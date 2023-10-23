426301233f5ab3af8013f2ae98ba0914c5639150
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-23 11:08:27 -0400

    add tailwind merge and clsx for merging className without conflict

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/utils/mergeTailwind";
+
 type ContainerProps = {
     children: React.ReactNode;
     maxWidth: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
@@ -8,7 +10,7 @@ type ContainerProps = {
 
 const Container = ({ children, maxWidth }: ContainerProps) => {
   return (
-    <div className={`mx-auto max-w-${maxWidth} 2xl:w-4/5 w-11/12`}>
+    <div className={cn('mx-auto 2xl:w-4/5 w-11/12', `max-w-${maxWidth}`)}>
       {children}
     </div>
   )
