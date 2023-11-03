0fb34be9826f711e3ad4385b21674c752c9f3900
Martin Tsang <tsangmartin1225@gmail.com>
2023-11-03 17:37:44 -0400

    fix analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,9 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
         </Head>
         <Layout>
           <Component {...pageProps} />
-          <Analytics />
         </Layout>
       </div>
+      <Analytics />
     </ThemeProvider>
   );
 }
